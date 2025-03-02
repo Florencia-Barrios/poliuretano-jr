b3875880f3994314175cf64dab4a9209d26b32f2
Florencia Barrios <florencianb@ext.inditex.com>
2025-03-03 00:34:27 +0100

    subiendo proyecto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Menú Responsive
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+});
